9f7d8fd296505a35e761382e3e71a19054476508
thoson <thoson.it@gmail.com>
2022-10-07 16:59:15 +0700

    add agora

---
+++ lib/configs/app_configs.dart
@@ -0,0 +1,11 @@
+class AppConfigs {
+  AppConfigs._();
+
+  static const appId = "095d2c52fb944bb19d935e03660f460b";
+  static const channel = "thoson.it";
+
+  static const liverToken =
+      "007eJxTYOh6N2XdLS3hdduvOTB/jbGvVTUS3aYm9f2he97Bwt1n7ucpMBhYmqYYJZsapSVZmpgkJRlaplgam6YaGJuZGaSZmBkkzXxrnyxp75gcNO0/CyMDBIL4nAwlGfnF+Xl6mSUMDAA0FCKw";
+  static const viewerToken =
+      "007eJxTYLiz4LT7zE29O2W7CnSbNzVz82Yn/buklx++6wWjVtc6lUkKDAaWpilGyaZGaUmWJiZJSYaWKZbGpqkGxmZmBmkmZgZJ+9/aJ9vbOyYzSO9jYIRCEJ+ToSQjvzg/Ty+zhIEBAO0IIT4=";
+}
---
+++ lib/utils/user_type.dart
@@ -0,0 +1,4 @@
+enum UserType {
+  liver,
+  viewer,
+}
